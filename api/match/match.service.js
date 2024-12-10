9a821b8d4545167c1952e0b046bab16b5080e27f
OriChaiMatan <ori.chaimatan@gmail.com>
2024-12-10 19:35:32 +0200

    Connect MatchesService to MongoDB

--- api/match/match.service.js
+++ api/match/match.service.js
@@ -1,46 +1,72 @@
 import { utilService } from "../../services/util.service.js"
+import { logger } from "../../services/logger.service.js"
+import { dbService } from "../../services/db.service.js"
+import mongodb from 'mongodb'
+const {ObjectId} = mongodb
 
-const futureMatches = utilService.readJsonFile('data/future-matches.json')
-const pastMatches = utilService.readJsonFile('data/past-matches.json')
+const previousMatchesCollectionName = 'previous-matches'
+const futureMatchesCollectionName = 'future-matches'
 
 export const _matchService = {
     getPastGames,
-    getFutureGames, 
-    // getGamesByDate,
+    getFutureGames,
     getPastMatchById,
     getFutureMatchById,
 }
 
 async function getPastGames() {
     try {
-        return pastMatches
+        const criteria = _buildCriteria()
+        const collection = await dbService.getCollection(previousMatchesCollectionName)
+        const matchCursor = await collection.find(criteria)
+
+        const previousMatches = await matchCursor.toArray()
+        return previousMatches
     } catch (err) {
+        logger.error(err)
         throw err
     }
 }
 
 async function getFutureGames() {
     try {
+        const criteria = _buildCriteria()
+        const collection = await dbService.getCollection(futureMatchesCollectionName)
+        const matchCursor = await collection.find(criteria)
+
+        const futureMatches = await matchCursor.toArray()
         return futureMatches
     } catch (err) {
+        logger.error(err)
         throw err
     }
 }
 
 async function getPastMatchById(matchId) {
     try {
-        const pastMatch = pastMatches.find(match => match.match_id === matchId)
+        const collection = await dbService.getCollection(previousMatchesCollectionName)
+        const pastMatch = collection.findOne({ match_id: matchId })
+        if (!pastMatch) throw `Couldn't find league with _id ${matchId}`
         return pastMatch
     } catch (err) {
+        logger.error(`while finding stay ${matchId}`, err)
         throw err
     }
 }
 
 async function getFutureMatchById(matchId) {
     try {
-        const futureMatch = futureMatches.find(match => match.match_id === matchId)
+        const collection = await dbService.getCollection(futureMatchesCollectionName)
+        const futureMatch = collection.findOne({ match_id: matchId })
+        if (!futureMatch) throw `Couldn't find league with _id ${matchId}`
         return futureMatch
     } catch (err) {
+        logger.error(`while finding stay ${matchId}`, err)
         throw err
     }
+}
+
+function _buildCriteria() {
+    const criteria = {}
+    return criteria
 }
