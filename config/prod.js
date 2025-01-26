d51c73e92c467e127155811360548b6b25352643
OriChaiMatan <ori.chaimatan@gmail.com>
2025-01-26 21:18:41 +0200

    Update config file

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,12 @@
-const MongoDB_URL = process.env.MONGO_DB_URL
-const MongoDB_DB_NAME = process.env.MONGO_DB_NAME
+// const MongoDB_URL = process.env.MONGO_DB_URL
+// const MongoDB_DB_NAME = process.env.MONGO_DB_NAME
+
+// export default {
+//     dbURL: MongoDB_URL,
+//     dbName: MongoDB_DB_NAME,
+// }
 
 export default {
-    dbURL: MongoDB_URL,
-    dbName: MongoDB_DB_NAME,
+    dbURL: 'mongodb+srv://orichaimatan:Lry9uOzZPXpu9Bgt@betting-app.bgnz2.mongodb.net/',
+    dbName: '1x2pro',
 }
--- services/db.service.js
+++ services/db.service.js
@@ -30,10 +30,7 @@ async function connect() {
     if (dbConn) return dbConn
     try {
         // console.log("config.dbURL", config.dbURL)
-        const client = await MongoClient.connect(config.dbURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        const client = await MongoClient.connect(config.dbURL)
         const db = client.db(config.dbName)
         dbConn = db
         logger.info('Connected to DB')
