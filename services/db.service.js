bce13afd30d9a27f643d74a0d8744034229f2685
OriChaiMatan <ori.chaimatan@gmail.com>
2025-01-26 20:55:38 +0200

    Fix: update MongoDB connection options with useNewUrlParser and useUnifiedTopology

--- services/db.service.js
+++ services/db.service.js
@@ -30,7 +30,10 @@ async function connect() {
     if (dbConn) return dbConn
     try {
         // console.log("config.dbURL", config.dbURL)
-        const client = await MongoClient.connect(config.dbURL)
+        const client = await MongoClient.connect(config.dbURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         const db = client.db(config.dbName)
         dbConn = db
         logger.info('Connected to DB')
